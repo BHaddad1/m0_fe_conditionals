231b01feaa4ffd6b37700accfa870120c98b19da
Blanche Haddad <blancherachel@gmail.com>
2022-09-21 15:54:05 -0600

    Add my work

---
+++ codeChallenges/challenge1.rb
@@ -0,0 +1,13 @@
+## Mild
+
+Write a JavaScript program that defines a variable that stores an Number.
+The program should print out the String "even" if the Number is even,
+and the String "odd" if the Number is odd.
+
+var number = 30
+if (number /=2) {
+  console.log("Even")
+}
+else if (number !/=2) {
+  console.log("odd")
+}
